76e002c9c54066792be7bd2ee9e65f7e739aa8f8
t-kinoshita <t-kinoshita@ttc-net.co.jp>
2018-05-03 22:33:24 +0900

    弾の追加

--- game.rb
+++ game.rb
@@ -3,10 +3,13 @@ require 'dxruby'
 image = Image.load('data.png')  # data.pngを読み込む
 x = 0
 y = 0
+s = 0
+t = 0
 
 Window.loop do
   x = x + Input.x  # x座標の更新
   y = y + Input.y  # y座標の更新
-
+  t = (t + 3)
   Window.draw(x, y, image)  # data.pngを描画する
+  Window.draw(s, t, image)
 end
